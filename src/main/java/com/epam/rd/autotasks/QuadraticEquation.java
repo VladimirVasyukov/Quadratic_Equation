26c097e4df18705505953ec55b789fce1e681096
Vladimir Vasyukov <vladimir.vasyukov7@gmail.com>
2021-09-18 15:27:18 +0300

    Java Fundamentals - Quadratic Equation

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -1,18 +1,36 @@
 package com.epam.rd.autotasks;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
-
+/**
+ * 04_Java_Fundamentals - Vladimir Vasyukov
+ * Application's entry point, use it to demonstrate code execution
+ */
 public class QuadraticEquation {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        final byte TWO = 2;
+        final byte FOUR = 4;
+        Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8.name());
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-
-
-
+        double d;
+        double x1;
+        double x2;
+        if (a != 0) {
+            d = b * b - FOUR * a * c;
+            if (d > 0) {
+                x1 = (-b - Math.sqrt(d)) / (TWO * a);
+                x2 = (-b + Math.sqrt(d)) / (TWO * a);
+                System.out.printf("%s %s", x1, x2);
+            } else if (d == 0) {
+                x1 = -b / (TWO * a);
+                System.out.println(x1);
+            } else {
+                System.out.print("no roots");
+            }
+        }
     }
-
 }
